a7e4b3b2d86e9babcc8f1b7237399450abe3f62a
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-08-26 22:39:28 +0000

    writing request_data tests

--- src/apis/vagas/vagas_api_test.py
+++ src/apis/vagas/vagas_api_test.py
@@ -1,8 +1,11 @@
-import unittest
+from unittest import TestCase
+from unittest.mock import MagicMock, patch, create_autospec
 from ...utils.json_utils import JsonUtils as JU
 
+import src.apis.vagas.vagas_api as VA
 
-class VagasApiTests(unittest.TestCase):
+
+class VagasApiTests(TestCase):
     def setUp(self):
         self.api_url, self.options = JU.read_json(
             'src/secrets/vagas_api_secret.json').values()
@@ -10,8 +13,10 @@ class VagasApiTests(unittest.TestCase):
     def tearDown(self):
         return super().tearDown()
 
-    def test_vagas_api_credentials(self):
-        with self.subTest():
-            self.assertGreater(len(self.api_url),  0, 'Api_url not supplied!')
-        with self.subTest():
-            self.assertGreater(len(self.options), 0, 'Options not supplied!')
+    def test_credentials(self):
+        self.assertGreater(len(self.api_url),  0, 'Api_url not supplied!')
+        self.assertGreater(len(self.options), 0, 'Options not supplied!')
+
+    def test_get_request(self):
+        VA.request_data = MagicMock(return_value=3)
+        self.assertEqual(3, VA.request_data(self.api_url, self.options))
