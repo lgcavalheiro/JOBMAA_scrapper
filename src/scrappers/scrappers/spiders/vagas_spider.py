c884448f94344ff3f29bf334341973c70477ae1b
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-08-30 01:44:40 +0000

    mvp for scrapy scrapper done

--- src/scrappers/scrappers/spiders/vagas_spider.py
+++ src/scrappers/scrappers/spiders/vagas_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 class VagasSpider(scrapy.Spider):
@@ -12,14 +13,29 @@ class VagasSpider(scrapy.Spider):
         self.links = []
 
     def parse(self, response):
-        partial_links = response.css(
-            'h2[class=cargo] a::attr(href)').extract()
+        partial_links = response.css('h2[class=cargo] a')
         self.links.extend(partial_links)
-        next_page = response.css(
-            'a.btMaisVagas::attr(data-url)').extract()
+        next_page = response.css('a.btMaisVagas::attr(data-url)').extract()
+
         if len(next_page) is not 0:
-            nextp = f'https://www.vagas.com.br{next_page[0]}'
-            yield scrapy.Request(nextp, callback=self.parse)
+            next_page = f'https://www.vagas.com.br{next_page[0]}'
+            yield response.follow(next_page, callback=self.parse)
         else:
             print('LINKS: ', self.links)
             print('LENGTH: ', len(self.links))
+            yield from response.follow_all(self.links, callback=self.parse_job)
+
+    def parse_job(self, response):
+        def get_job_description():
+            nodes = response.css('div.job-tab-content *::text').getall()
+            return ' '.join([re.sub(r'[\n\r\xa0]', '', node).strip() for node in nodes])
+
+        print(f'Targeting: {response.url}')
+
+        yield {
+            "job_title": response.css('h1.job-shortdescription__title::text').get().strip(),
+            "company_name": response.css('h2.job-shortdescription__company::text').get().strip(),
+            "hierarchy": ''.join(response.css('span.job-hierarchylist__item span::text').getall()).strip().replace(' ', '').replace('\n', ''),
+            "job_url": response.url,
+            "job_description": get_job_description()
+        }
