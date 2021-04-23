e7d925e7845111e9b9802868daa024c20443751a
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2021-04-23 19:53:30 -0300

    get_job_info refactoring + starting db integration

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 test:
 	python3 -m unittest discover -t . -s src/ -p "*_test.py"
 
+local_db:
+	cd ./local_db \
+	&& docker build . -t jobmaa_db \ 
+	&& docker run -p 5432:5432 jobmaa_db
+
 vagas_spider:
 	cd ./src \
-	&& scrapy crawl VagasSpider -o ../../results/vagas_spider_raw_results.json
+	&& scrapy crawl VagasSpider -o ../../results/vagas_spider_raw_results.csv
--- src/scrappers/spiders/vagas_spider.py
+++ src/scrappers/spiders/vagas_spider.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from hashlib import md5
 import scrapy
 import re
 
@@ -6,7 +7,7 @@ import re
 class VagasSpider(scrapy.Spider):
     name = 'VagasSpider'
     start_urls = [
-        'https://www.vagas.com.br/vagas-de-desenvolvedor?a[]=24&c[]=Rio+de+Janeiro&c[]=S%C3%A3o+Paulo&h[]=30&h[]=40&ordenar_por=mais_recentes'
+        'https://www.vagas.com.br/vagas-de-desenvolvedor?c[]=Rio+de+Janeiro&c[]=S%C3%A3o+Paulo&ordenar_por=mais_recentes'
     ]
 
     def __init__(self, name=None, **kwargs):
@@ -25,8 +26,15 @@ class VagasSpider(scrapy.Spider):
             yield from response.follow_all(self.links, callback=self.parse_job)
 
     def parse_job(self, response):
-        def get_job_info(selector):
-            return ' '.join([re.sub(r'[\n\r\xa0]', '', node).strip() for node in response.css(selector).getall()]).strip()
+        def get_job_info(selector, shouldStringify=True):
+            try:
+                data = [re.sub(r'[\n\r\xa0]', '', node).strip()
+                        for node in response.css(selector).getall()]
+                if(shouldStringify):
+                    return ' '.join(data)
+                return data
+            except:
+                return '[DATA PROCESS ERROR] @ get_job_info'
 
         def get_job_wage_and_location():
             try:
@@ -51,7 +59,9 @@ class VagasSpider(scrapy.Spider):
                 raw_date = response.css(
                     'li.job-breadcrumb__item--published *::text').getall()[1]
                 date = raw_date.replace('\n', '').strip().split()[-1]
-                if(date == 'ontem'):
+                if(date == 'hoje'):
+                    return datetime.now().strftime("%d/%m/%Y")
+                elif(date == 'ontem'):
                     return (datetime.now() - timedelta(days=1)).strftime("%d/%m/%Y")
                 elif(date == 'dias'):
                     date = raw_date.replace('\n', '').strip().split()[-2]
@@ -62,19 +72,26 @@ class VagasSpider(scrapy.Spider):
                 return '[DATA PROCESS ERROR] @ get_job_publish_date'
 
         wage, location = get_job_wage_and_location()
+        hierarchy = get_job_info('span.job-hierarchylist__item span::text')
+        description = get_job_info('div[data-testid=JobDescription] *::text')
+
+        # hash used to prevent inter-source duplicates
+        job_hash = ''.join([re.sub(r'\W', '', item)
+                           for item in [hierarchy, description]])
 
         yield {
-            "source_id": response.url.split('/')[4],
-            "job_title": get_job_info('h1.job-shortdescription__title::text'),
-            "company_name": get_job_info('h2.job-shortdescription__company::text'),
-            "hierarchy": get_job_info('span.job-hierarchylist__item span::text'),
-            "wage": wage,
-            "location": location,
-            "publish_date": get_job_publish_date(),
-            "job_url": response.url,
-            "job_benefits": get_job_info('div.job-benefits *::text'),
-            "job_description": get_job_info('div[data-testid=JobDescription] *::text'),
-            "company_info": get_job_info('div[data-testid=JobCompanyPresentation] *::text'),
-            "extraction_timestamp": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-            "source": "vagas.com"
+            "SOURCE_ID": response.url.split('/')[4],
+            "JOB_HASH": md5(job_hash.encode()).hexdigest(),
+            "TITLE": get_job_info('h1.job-shortdescription__title::text'),
+            "COMPANY_NAME": get_job_info('h2.job-shortdescription__company::text'),
+            "HIERARCHY": hierarchy,
+            "WAGE": wage,
+            "LOCATION": location,
+            "PUBLISH_DATE": get_job_publish_date(),
+            "URL": response.url,
+            "BENEFITS": get_job_info('li.job-benefits__list-item span::text', False),
+            "DESCRIPTION": description,
+            "COMPANY_INFO": get_job_info('div[data-testid=JobCompanyPresentation] *::text'),
+            "EXTRACTION_TIMESTAMP": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            "SOURCE": "vagas.com"
         }
